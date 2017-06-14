5caf95bd40f2ee3cbeebf1224a35587d8c724f61
Jung YunHo <saijdu2198@gmail.com>
2017-06-13 22:28:40 -0700

    add CommentLike model

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -7,8 +7,30 @@ class Post(models.Model):
     photo = models.ImageField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    like_users = models.ManyToManyField(User, related_name='like_posts')
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts',
+        through='PostLike',
+    )
     tags = models.ManyToManyField('Tag')
+
+    def add_comment(self, user, content):
+        return self.comment_set.create(author=user, content=content)
+
+    def add_tag(self, tag_name):
+        tag, tag_created = Tag.objects.get_or_create(name=tag_name)  # check !
+        self.tags.add(tag)
+
+    @property
+    def like_count(self):
+        return self.like_users.count()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+
 
 
 class Comment(models.Model):
@@ -17,6 +39,17 @@ class Comment(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(
+        User,
+        through='CommentLike',
+        related_name='like_comments',
+    )
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Tag(models.Model):
@@ -24,3 +57,11 @@ class Tag(models.Model):
 
     def __str__(self):
         return 'Tag({})'.format(self.name)
+
+
+# 1. 중간자 모델을 만든다 (through통해)
+# 2. 새로 만든 중간자 모델에 메타를 써서 db이름을 기존의 m2m db 이름과 같게 만든다
+# 3. migrate --fake로 덮어씌운다
+# 4. 메타 지우고 추가하고싶은 필드(열)을 추가하고
+# 5. 다시 makemigrations migrate한다
+
